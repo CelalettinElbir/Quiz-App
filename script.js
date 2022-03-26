eab52b79caa43c74e77e433458d8b04e093e5f01
Celalettin Elbir <celallelbir@hotmail.com>
2022-03-26 21:57:18 +0300

    questions js array created.

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-console.log("deneme");
+
+const questions = [
+    {
+        Title:"Soru",
+        "a":"cevap a",
+        "b":"cevap b",
+        "c":"cevap c",
+        "d":"cevap d",
+
+    },
+
+    {
+        Title:"Soru 2",
+        "a":"cevap a",
+        "b":"cevap b",
+        "c":"cevap c",
+        "d":"cevap d",
+
+    },
+
+    {
+        Title:"Soru 3 ",
+        "a":"cevap a",
+        "b":"cevap b",
+        "c":"cevap c",
+        "d":"cevap d",
+
+    },
+    {
+        Title:"Soru 4",
+        "a":"cevap a",
+        "b":"cevap b",
+        "c":"cevap c",
+        "d":"cevap d",
+
+    },
+    {
+        Title:"Soru 5",
+        "a":"cevap a",
+        "b":"cevap b",
+        "c":"cevap c",
+        "d":"cevap d",
+
+    }
+
+
+
+
+]
+console.log(questions)
